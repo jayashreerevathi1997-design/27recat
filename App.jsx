6e93847ad8a385fbe364c85dcb48f934663cb455
jayashreerevathi1997-design <jayashreerevathi1997@gmail.com>
2026-01-30 18:02:06 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+/* import Button from "./components/button.jsx"; 
+function App() { 
+  return ( 
+    <div> 
+      <Button label="Click Me" /> 
+      <Button label="Submit" /> 
+    </div> 
+  ); 
+}
+  export default App; */
+ 
+import ReactTasks from "./components/27.jsx";
+
+function App() {
+  return (
+    <div>
+      <ReactTasks />
+    </div>
+  );
+}
+
+export default App;
